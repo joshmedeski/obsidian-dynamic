56ddc21a613cf5cd75c908687669b208f6817c59
Josh Medeski <josh.medeski@gmail.com>
2025-08-02 09:08:21 -0500

    🎨 Format monorepo with ultracite

--- plugins/obsidian-vertical-video-widget/src/main.ts
+++ plugins/obsidian-vertical-video-widget/src/main.ts
@@ -1,59 +1,59 @@
-import { Plugin } from "obsidian";
+import { Plugin } from 'obsidian';
 import {
-	VerticalVideoWidgetView,
-	VIEW_TYPE_VERTICAL_VIDEO_WIDGET,
-} from "./widgetView";
+  VerticalVideoWidgetView,
+  VIEW_TYPE_VERTICAL_VIDEO_WIDGET,
+} from './widgetView';
 
 export default class VerticalVideoPlugin extends Plugin {
-	async onload() {
-		// Register the dynamic widget view
-		this.registerView(
-			VIEW_TYPE_VERTICAL_VIDEO_WIDGET,
-			(leaf) => new VerticalVideoWidgetView(leaf),
-		);
+  async onload() {
+    // Register the dynamic widget view
+    this.registerView(
+      VIEW_TYPE_VERTICAL_VIDEO_WIDGET,
+      (leaf) => new VerticalVideoWidgetView(leaf)
+    );
 
-		// Auto-activate the widget in the right sidebar
-		// this.app.workspace.onLayoutReady(async () => {
-		// 	const leaf = this.app.workspace.getLeftLeaf(false);
-		// 	if (leaf) {
-		// 		await this.activateView();
-		// 	}
-		// });
+    // Auto-activate the widget in the right sidebar
+    // this.app.workspace.onLayoutReady(async () => {
+    // 	const leaf = this.app.workspace.getLeftLeaf(false);
+    // 	if (leaf) {
+    // 		await this.activateView();
+    // 	}
+    // });
 
-		// Add command to toggle the dynamic widget
-		this.addCommand({
-			id: "open-dynamic-widget",
-			name: "Open Dynamic Widget",
-			callback: () => {
-				this.activateView();
-			},
-		});
-	}
+    // Add command to toggle the dynamic widget
+    this.addCommand({
+      id: 'open-dynamic-widget',
+      name: 'Open Dynamic Widget',
+      callback: () => {
+        this.activateView();
+      },
+    });
+  }
 
-	async activateView() {
-		// Remove any existing instances
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_VERTICAL_VIDEO_WIDGET);
+  async activateView() {
+    // Remove any existing instances
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE_VERTICAL_VIDEO_WIDGET);
 
-		// Create and activate the view in right sidebar
-		const leaf = this.app.workspace.getRightLeaf(false);
-		if (!leaf) return;
+    // Create and activate the view in right sidebar
+    const leaf = this.app.workspace.getRightLeaf(false);
+    if (!leaf) return;
 
-		await leaf.setViewState({
-			type: VIEW_TYPE_VERTICAL_VIDEO_WIDGET,
-			active: true,
-		});
+    await leaf.setViewState({
+      type: VIEW_TYPE_VERTICAL_VIDEO_WIDGET,
+      active: true,
+    });
 
-		// Ensure the view is visible
-		const leaves = this.app.workspace.getLeavesOfType(
-			VIEW_TYPE_VERTICAL_VIDEO_WIDGET,
-		);
-		if (leaves.length > 0) {
-			this.app.workspace.revealLeaf(leaves[0]);
-		}
-	}
+    // Ensure the view is visible
+    const leaves = this.app.workspace.getLeavesOfType(
+      VIEW_TYPE_VERTICAL_VIDEO_WIDGET
+    );
+    if (leaves.length > 0) {
+      this.app.workspace.revealLeaf(leaves[0]);
+    }
+  }
 
-	onunload() {
-		// Clean up the view when plugin is disabled
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_VERTICAL_VIDEO_WIDGET);
-	}
+  onunload() {
+    // Clean up the view when plugin is disabled
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE_VERTICAL_VIDEO_WIDGET);
+  }
 }
